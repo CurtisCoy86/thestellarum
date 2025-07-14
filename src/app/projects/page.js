c875ab713dcedd7e3beb2627948aeced5a8fd721
Stellar <archwizard822@gmail.com>
2025-07-14 18:00:10 -0400

    Fixed unescaped apostrophe in projects page

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -2,7 +2,7 @@ export default function ProjectsPage({children}) {
   return (
     <main className="p-8">
       <h1 className="text-3xl font-bold">Projects</h1>
-      <p className="mt-4">Here you'll find Auress_Edge and all our future creations, shining in the digital sky.</p>
+      <p className="mt-4">Here you&apos;ll find Auress_Edge and all our future creations, shining in the digital sky.</p>
     </main>
   );
 }
